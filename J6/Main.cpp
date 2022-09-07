f73ae7f48ef8fd39daf4fa5ef9b71e1e15c4d70a
shiba28 <8128kanzensu@gmail.com>
2022-09-07 16:02:27 +0900

    プロジェクト ファイルを追加します。

---
+++ J6/Main.cpp
@@ -0,0 +1,29 @@
+﻿# include "Common.hpp"
+# include "Title.hpp"
+# include "Game.hpp"
+
+void Main()
+{
+	FontAsset::Register(U"TitleFont", FontMethod::MSDF, 50, U"example/font/RocknRoll/RocknRollOne-Regular.ttf");
+	FontAsset(U"TitleFont").setBufferThickness(4);
+	FontAsset::Register(U"Menu", FontMethod::MSDF, 40, Typeface::Medium);
+	FontAsset::Register(U"Ranking", 40, Typeface::Heavy);
+	FontAsset::Register(U"GameScore", 30, Typeface::Light);
+	AudioAsset::Register(U"Brick", GMInstrument::Woodblock, PianoKey::C5, 0.2s, 0.1s);
+
+	App manager;
+	manager.add<Title>(State::Title);
+	manager.add<Game>(State::Game);
+
+
+	// ゲームシーンから開始したい場合はこのコメントを外す
+	//manager.init(State::Game);
+
+	while (System::Update())
+	{
+		if (not manager.update())
+		{
+			break;
+		}
+	}
+}
