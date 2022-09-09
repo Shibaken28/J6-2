aa84d4b7b0617740eb101db5a1f7c90f29d663e0
shiba28 <8128kanzensu@gmail.com>
2022-09-09 15:46:41 +0900

    WIP

---
+++ J6/EntityObject.hpp
@@ -0,0 +1,20 @@
+﻿# pragma once
+# include "Common.hpp"
+
+class GameObject {
+public:
+	bool isAlive = true;
+	bool isRigid = false;
+	Vec2 position{ 0,0 };
+	Vec2 Velocity{ 0,0 };
+	Size size{ 64,64 };
+	Point considerVelocity;
+public:
+	virtual bool isHit(GameObjectNode) = 0;
+	virtual void update() = 0;
+	virtual GameObjectNode getNode() = 0;
+	virtual void draw() const = 0;
+	Rect rect() {
+		return Rect(position.asPoint(), size);
+	}
+};
---
+++ J6/GameObject.hpp
@@ -0,0 +1,29 @@
+﻿# pragma once
+# include "Common.hpp"
+
+
+namespace MyGame {
+
+	bool hasCommonRange(double l0, double r0, double l1, double r1);
+	bool isMovingRectHit(const Rect& rect0, const Point& var0, const Rect& rect1, const Point& var1, bool& isHitX, bool& isHitY, double& hitTime, Point& revert);
+	
+	enum class GameObjectType :int {
+		Player, Needle
+	};
+
+	using objectShape = std::variant<Triangle>;
+
+	struct GameObjectNode {
+		GameObjectType objectType;
+		Array<objectShape> shapes;
+		GameObjectNode();
+		GameObjectNode(GameObjectType type);
+	};
+
+	class GameObject {
+	public:
+		virtual void update() = 0;
+	};
+
+}
+
